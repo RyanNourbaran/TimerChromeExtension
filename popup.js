db75a81d7c8fa5a6d4eddfb842bbb57caa7d8da0
Ryan Nourbaran <ryan.nourbaran@hotmail.com>
2018-03-02 23:39:24 -0500

    added reset and stop buttons

--- popup.js
+++ popup.js
@@ -1,13 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  var time = document.getElementById("time");
-  document.getElementById("startBtn").addEventListener("click", function() {
-    var decreaseTime = setInterval(function() {
-      if (time.textContent != 0) {
-        time.innerHTML = time.textContent - 1;
-      }
-    }, 1000);
-    if (time.textContent == 0) {
-      clearInterval(decreaseTime);
-    }
-  });
+    var time = document.getElementById("time");
+    var startStopBtn = document.getElementById("resetBtn");
+    var startTime = time.textContent;
+
+    startStopBtn.addEventListener("click", () => {
+        time.innerHTML = startTime;
+        clearInterval(decreaseTime);
+    });
+
+    startStopBtn.addEventListener("click", () => {
+        document.getElementById("startStopBtn").innerHTML = "Stop";
+        var decreaseTime = setInterval(() => {
+            if (time.textContent != 0 && started) {
+                time.innerHTML = time.textContent - 1;
+            }
+        }, 1000);
+
+        if (time.textContent == 0) {
+            clearInterval(decreaseTime);
+        }
+        startStopBtn.addEventListener("click", () => {
+            document.getElementById("startStopBtn").innerHTML = "Start";
+            clearInterval(decreaseTime);
+        });
+    });
 });
