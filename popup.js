74442e439f6d1a85a9e80ca674b6d102846bd486
Ryan Nourbaran <ryan.nourbaran@hotmail.com>
2018-03-03 00:03:21 -0500

    stop,start,reset work

--- popup.js
+++ popup.js
@@ -1,27 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
     var time = document.getElementById("time");
-    var startStopBtn = document.getElementById("startStopBtn");
     var startTime = time.textContent;
+    var started = false;
+    var decreaseTime = undefined;
 
     document.getElementById("resetBtn").addEventListener("click", () => {
         time.innerHTML = startTime;
         clearInterval(decreaseTime);
     });
 
-    startStopBtn.addEventListener("click", () => {
-        startStopBtn.innerHTML = "Stop";
-        var decreaseTime = setInterval(() => {
-            if (time.textContent != 0 && started) {
-                time.innerHTML = time.textContent - 1;
-            }
-        }, 1000);
+    document.getElementById("startStopBtn").addEventListener("click", () => {
+        if (started) {
+            started = false;
+            document.getElementById("startStopBtn").innerHTML = "Start";
 
-        if (time.textContent == 0) {
+            clearInterval(decreaseTime);
+            decreaseTime = undefined;
+        } else if (!decreaseTime) {
+            started = true;
+            document.getElementById("startStopBtn").innerHTML = "Stop";
+            decreaseTime = setInterval(() => {
+                if (time.textContent != 0 && started) {
+                    time.innerHTML = time.textContent - 1;
+                }
+            }, 1000);
+        }
+
+        if (time.textContent == 0 || !started) {
             clearInterval(decreaseTime);
         }
-        startStopBtn.addEventListener("click", () => {
-            document.getElementById("startStopBtn").innerHTML = "Start";
-            clearInterval(decreaseTime);
-        });
     });
 });
